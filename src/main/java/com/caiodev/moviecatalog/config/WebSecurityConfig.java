c47acdc8625680284ff1249c890f944f45260b47
ccaiomacedo <caio.macedo-mtm@hotmail.com>
2022-07-05 15:38:13 -0300

    Implementing cryptography

--- src/main/java/com/caiodev/moviecatalog/config/WebSecurityConfig.java
+++ src/main/java/com/caiodev/moviecatalog/config/WebSecurityConfig.java
@@ -1,8 +1,19 @@
 package com.caiodev.moviecatalog.config;
 
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class WebSecurityConfig  {
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/actuator/**");
+    }
 
 }
---
+++ src/main/java/com/caiodev/moviecatalog/services/UserService.java
@@ -0,0 +1,8 @@
+package com.caiodev.moviecatalog.services;
+
+import org.springframework.stereotype.Service;
+
+public class UserService {
+
+
+}
